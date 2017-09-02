c94e795612380344888e89550ddd0251ac5df79a
nath <miiluu_nathy@hotmail.com>
2017-09-02 16:24:32 -0500

    mi primer commit

---
+++ prnath.py
@@ -0,0 +1,9 @@
+fruits = ['apple','orange','pear']
+
+def fruitsName(name):
+    print ("Hi! " + name)
+    for fruit in fruits:
+        if fruits[0] == 'apple':
+            print("i love")
+        print('hello! I am a' + fruit)
+fruitsName('nath')
